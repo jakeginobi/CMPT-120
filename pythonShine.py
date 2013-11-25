f4444d261c45f84858babcf0cfc4c646572ffdc2
landalex <AlexLand@Alexs-MacBook-Pro.local>
2013-11-24 21:45:32 -0800

    Added compulsory code

--- pythonShine.py
+++ pythonShine.py
@@ -48,15 +48,78 @@ def biomeDraw(fillColor):
         t.left(90)
         t.end_fill()
         t.forward(70)
+
+def playerDraw():
+    t.fillcolor("red")
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
         
 def drawBoard(catastropheNumber):
     biomeDraw() * (8 - catastropheNumber)
-    
-    
-    
 
-gameStart()
+def drawPlayer(biomeNumber):
+    t.penup()
+    t.forward(25)
+    t.forward((biomeNumber - 1) * 50)
+    playerDraw()
 
-biomeGenerator(godMode)
-print godMode
-print biomePosition
+def read_string_list_from_file(the_file):
+    '''
+    CODE PROVIDED TO INCORPORATE
+    
+    <file-name including extension .txt>(String) --> List of strings
+    
+    Assumptions:
+    1) the_file is in the same directory as this program 
+    2) the_file contains one biome data per line 
+    3) after each biome in the file  there is a return (
+       so that the next biome is in the next line), and also
+      there is (one single) return after the last biome
+      in the_file
+
+    to call or invoke this function:
+    listStrings = read_string_list_from_file(<file-name.txt in quotes>)
+    '''
+    
+    fileRef = open(the_file,"r") # opening file to be read
+    localList=[]
+    for line in fileRef:
+        string = line[0:len(line)-1]  # eliminates trailing '\n'
+                                      # of each line 
+                                    
+        localList.append(string)  # adds string to list
+        
+    fileRef.close()  
+        
+    #........
+    #print "\n JUST TO TRACE, the local list of strings is:\n"
+    #for element in localList:
+    #    print element
+    #print
+    #........
+        
+    return localList
+
+
+def create_lists_board(listStrings):
+    
+    '''
+    RECOMMENDED
+    '''
+    # your code...   
+    
+    return #.... as many lists as needed to represent the board
+           # one for the diamonds values, etc.
+
+
+
+def show_board(mssg):
+    '''
+    RECOMMENDED
+    '''
+    print "\nShowing board... " + mssg 
+    print "\n The board at this point contains..."
+
+    # your code... 
+
